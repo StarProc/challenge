eaf060682c2e712c90dd78f15b559612d649fd65
sckim <sckim@enliple.com>
2021-04-06 10:54:00 +0900

    init

---
+++ src/test/java/com/sckim/starproc/StarprocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sckim.starproc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarprocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
